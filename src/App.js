8a3aa31d176b205343a81c7bb3a7542263d86681
DuncanBScruggs <duncanbscruggs@gmail.com>
2020-10-14 16:38:20 -0400

    moves img into public and have a working one page in react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import "./img"
 
 function App() {
   return (
@@ -33,12 +32,113 @@ function App() {
       <div class="row">
           <div class="col-lg-12 p-0 pb-5">
               <div class="">
-                  <img class="d-block img-fluid" src="./img/masaimarasunset.jpg" alt="Acacia tree in the Masai Mara, Kenya"></img>
+                  <img class="d-block img-fluid" src="/img/masaimarasunset.jpg" alt="Acacia tree in the Masai Mara, Kenya"></img>
               </div>
 
           </div>
       </div>
+
+      <div class="row">
+            
+      
+            <div class="card text-center col-12">
+                <div class="card-header"><img src="./img/download.jpg" class="img-fluid"></img></div>
+                <div class="card-body">
+                    <h4 class="card-title">Pre-Work First Week</h4>
+                    <p class="card-text">I really did not know what to expect my first week of bootcamp at Awesome Inc and that is even after going through the intro to web development course. I really shouldn't have been surprised how fast we hit the ground running but I
+                        think it is a good way to learn, no BS just get to it. I struggled with getting my git setup talking to github at first but felt really good once i figured the flow out. Such a powerful tool that I am learning to use excites me
+                        at the possibilities! Added links on my blog to my projects and links pages and populated them with some content, while also touching up some css.
+                    </p>
+                </div>
+
+            </div>
+            
+            <div class="card text-center col-12">
+                <div class="card-header"><img src="./img/knapkin.jpg" class="img-fluid"></img></div>
+                <div class="card-body">
+                    <h4 class="card-title">Pre-Work Second Week</h4>
+                    <p class="card-text">Pseudo Coding is a super useful practice that i have used in the past but I never knew there was a more standardized formatting. I struggled a bit to remember the new formatting we learned this week but still need to practice. I am
+                        really excited to better convey my thoughts and ideas more clearly to others through the proper use of pseudo code format.
+                    </p>
+                </div>
+
+            </div>
+            
+            <div class="card text-center col-12">
+                <div class="card-header"><img src="./img/tenor.gif" class="img-fluid"></img></div>
+                <div class="card-body">
+                    <h4 class="card-title">Pre-Work Third week</h4>
+                    <p class="card-text">Had to do a lot of refreshing on my JavaScript skills this week. Spent way more time than I thought on the sum of all numbers from a to b than I thought since I had made one before in Java. I knew to use my Gauss formula which was
+                        right sometimes but failed to remember to use the MATH.abs function with it to make sure it was always giving the correct output! lots of time banging my head until recalling to use the function.</p>
+                </div>
+
+            </div>
+            
+            <div class="card text-center col-12">
+                <div class="card-header"><img src="./img/download.jpg" class="img-fluid"></img></div>
+                <div class="card-body">
+                    <h4 class="card-title">Pre-Work Fourth Week</h4>
+                    <p class="card-text">Fourth week and staring at the beginning of the in person meat and potatoes part of the class starting next week. I am excited but also nervous about next week and the fact that my plan to dual boot my laptop with macOS on it failed
+                        didn't help. I was able to put a virtual machine running Catalina but it was less than ideal. Finding a macbook air last minute definitely has alleviated a lot of that nervousness.</p>
+                    <p class="card-text">This week's projects took some research to work through. Specifically learning about the Date() function, other functions like .getHours(). Checking my math on how to accurately calculate days, hours, minutes, and seconds from milliseconds.
+                        The katas from this week really made me think and I wanted to try to solve them more efficiently and really think about my work flow. Specifically after I finished the array plus array kata I saw that there was a Concatenate function
+                        that could have been used.
+                    </p>
+
+                </div>
+
+            </div>
+
+            
+            <div class="card text-center col-12">
+                <div class="card-header"><img src="./img/download.jpg" class="img-fluid"></img></div>
+                <div class="card-body">
+                    <h4 class="card-title">First in person week</h4>
+                    <p class="card-text">
+                        WOW I haven not been getting up this early consistently in a long time! Honestly feels pretty good, although starting an hour later would be pretty nice hah.
+                        I feel genuinely productive and like I am making progress, even when I am stuck on something. Getting to know everyone and be able to talk to them about the things we are learning
+                        but also getting to know them as people has been wonderful!
+                    </p>
+                </div>
+            </div>
+
+           
+            <div class="card text-center col-12">
+                <div class="card-header"><img src="./img/download.jpg" class="img-fluid"></img></div>
+                <div class="card-body">
+                    <h4 class="card-title">Second week</h4>
+                    <p class="card-text">
+                        Good progress this week. finished the projects set for this week early on and started the tic tac toe for next week.
+                        I felt a lot of breakthroughs this week while working on tic tac toe.  I felt encouraged by my one on one with Ian and Justin.  
+                        I know I am in good hands with them and their guidance in our session has given me a lot to think about and improve but I never felt discouraged
+                        but infact uplifted by their evaluation, knowing they are looking out for all of us.
+                    </p>
+                </div>
+            </div>
+
+            
+            <div class="card text-center col-12">
+                <div class="card-header"><img src="./img/download.jpg" class="img-fluid"></img></div>
+                <div class="card-body">
+                    <h4 class="card-title">Third Week</h4>
+                    <p class="card-text">
+                       <ol>
+                           <li>online store</li>
+                           <li>to be decided</li>
+                           <li>im thinking</li>
+                        </ol>
+                        I find JavaScript very useful to be able to make my ideas become concrete.
+                         I am interested to learn more about class based programming. MVC has definitely helped me pseudocode better and thing through
+                         how i want my program to work. My current design pattern that i need to stop using is being a cowboy *bang* *bang* shooting from the hip.
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
     </div>
+
+
   </div>
     
   );
