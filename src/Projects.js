8d5ef3dc0790a174dcd511b8292d6ff3eaf28bde
DuncanBScruggs <duncanbscruggs@gmail.com>
2020-10-15 11:34:48 -0400

    setup different files for content

---
+++ src/Projects.js
@@ -0,0 +1,117 @@
+import React from "react";
+
+    <div class="row">
+    {/* <!-- Cards go here --> */}
+
+        {/* <!-- 1st card --> */}
+        <div class="card text-center col-12 col-sm-6 col-lg-3">
+            <div class="card-header"><img src="../img/githublogo.png" class="img-fluid" /></div>
+            <div class="card-body">
+                <h4 class="card-title"><a href="https://github.com/DuncanBScruggs?tab=repositories">Git Hub Repositories</a></h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.
+                </p>
+            </div>
+        </div>
+
+        {/* <!-- 2nd card --> */}
+        <div class="card text-center col-12 col-sm-6 col-lg-3">
+            <div class="card-header"><img src="../img/githublogo.png" class="img-fluid" /></div>
+            <div class="card-body">
+                <h4 class="card-title"><a href="https://github.com/DuncanBScruggs/markup">Markup Letter</a></h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.
+                </p>
+            </div>
+        </div>
+
+        {/* <!-- 3rd card --> */}
+        <div class="card text-center col-12 col-sm-6 col-lg-3">
+            <div class="card-header"><img src="../img/githublogo.png" class="img-fluid" /></div>
+            <div class="card-body">
+                <h4 class="card-title"><a href="https://github.com/DuncanBScruggs/content">Structuring a page of content</a></h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.
+                </p>
+            </div>
+        </div>
+
+        {/* <!-- 4th card --> */}
+        <div class="card text-center col-12 col-sm-6 col-lg-3">
+            <div class="card-header"><img src="../img/githublogo.png" class="img-fluid" /></div>
+            <div class="card-body">
+                <h4 class="card-title"><a href="https://github.com/DuncanBScruggs/pseudoCode">Pseudo Code</a></h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.
+                </p>
+            </div>
+        </div>
+
+        {/* <!-- 5th card --> */}
+        <div class="card text-center col-12 col-sm-6 col-lg-3">
+            <div class="card-header"><img src="../img/githublogo.png" class="img-fluid" /></div>
+            <div class="card-body">
+                <h4 class="card-title"><a href="https://github.com/DuncanBScruggs/sillystory">Silly Story generator</a></h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.
+                </p>
+            </div>
+        </div>
+
+        {/* <!-- 6th card --> */}
+        <div class="card text-center col-12 col-sm-6 col-lg-3">
+            <div class="card-header"><img src="../img/githublogo.png" class="img-fluid" /></div>
+            <div class="card-body">
+                <h4 class="card-title"><a href="https://github.com/DuncanBScruggs/mozillasplashpage">Mozilla splash page</a></h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.
+                </p>
+            </div>
+        </div>
+
+        {/* <!-- 7th card --> */}
+        <div class="card text-center col-12 col-sm-6 col-lg-3">
+            <div class="card-header"><img src="../img/githublogo.png" class="img-fluid" /></div>
+            <div class="card-body">
+                <h4 class="card-title"><a href="https://github.com/DuncanBScruggs/imagegallery">Image gallery</a></h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.
+                </p>
+            </div>
+        </div>
+
+        {/* <!-- 8th card --> */}
+        <div class="card text-center col-12 col-sm-6 col-lg-3">
+            <div class="card-header"><img src="../img/githublogo.png" class="img-fluid" /></div>
+            <div class="card-body">
+                <h4 class="card-title"><a href="https://github.com/DuncanBScruggs/digitalclock">Digital clock</a></h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.
+                </p>
+            </div>
+        </div>
+
+        {/* <!-- 9th card --> */}
+        <div class="card text-center col-12 col-sm-6 col-lg-3">
+            <div class="card-header"><img src="../img/githublogo.png" class="img-fluid" /></div>
+            <div class="card-body">
+                <h4 class="card-title"><a href="https://github.com/DuncanBScruggs/countdownTimer">Countdown Timer</a></h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.
+                </p>
+            </div>
+        </div>
+        {/* <!-- 10th card --> */}
+        <div class="card text-center col-12 col-sm-6 col-lg-3">
+            <div class="card-header"><img src="../img/githublogo.png" class="img-fluid" /></div>
+            <div class="card-body">
+                <h4 class="card-title"><a href="https://github.com/DuncanBScruggs/bouncing-balls">Bouncing Balls</a></h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.
+                </p>
+            </div>
+        </div>
+
+        {/* <!-- 11th card --> */}
+        <div class="card text-center col-12 col-sm-6 col-lg-3">
+            <div class="card-header"><img src="../img/githublogo.png" class="img-fluid" /></div>
+            <div class="card-body">
+                <h4 class="card-title"><a href="https://github.com/DuncanBScruggs/weather-app">Weather App</a></h4>
+                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam eu sem tempor, varius quam at, luctus dui. Mauris magna metus, dapibus nec turpis vel, semper malesuada ante.
+                </p>
+            </div>
+        </div>
+
+
+    {/* <!-- end of row --> */}
+    </div>
