377ecdcec2c24c35ccc823c9d24781e0a4e54681
DuncanBScruggs <duncanbscruggs@gmail.com>
2020-10-15 16:02:02 -0400

    working blog

---
+++ src/Components/NavBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function NavBar(props) {
+
+    return(
+        <nav class="navbar navbar-expand-lg navbar-dark brand fixed-top">
+             <div class="container">
+                 <a class="navbar-brand" href="#">Duncan Scruggs's Bootcamp Blog</a>
+                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
+                 </button>
+                 <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        {
+                            props.pages.map((item, index) => {
+                                return (
+                                    <li class="nav-item">
+                                        <a 
+                                            href={item.url}
+                                            onClick={() => props.setPage(index)} 
+                                            class={"nav-link " + (props.currentPage === index ? "active" : "")}>
+                                            {item.readableName}
+                                        </a>
+                                    </li>
+                                )
+                            })
+                        }
+                    </ul>
+                 </div>
+             </div>
+         </nav>
+    )
+}
+
+export default NavBar;
---
+++ src/Components/View.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import BlogPosts from './Components/BlogPosts';
+
+function View(){
+    
+}
+
+export default View;
